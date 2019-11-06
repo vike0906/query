bd01fb418ba7b55b4988659f1118b29837ca7ac9
vike0906 <liujialong688@outlook.com>
2019-11-06 23:39:03 +0800

    init

---
+++ src/main/java/com/vike/query/vo/Response.java
@@ -0,0 +1,32 @@
+package com.vike.query.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author: lsl
+ * @createDate: 2019/10/25
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Response<T> {
+
+    public static final int SUCCESS = 0;
+
+    public static final int ERROR = 2;
+
+    public static final String NULL = "";
+
+    private Integer code;
+
+    private String message;
+
+    private T data;
+
+    public Response(Integer code, String msg){
+        this.code = code;
+        this.message = msg;
+    }
+}
